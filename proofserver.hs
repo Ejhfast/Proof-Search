3f2e770573dbb8af9ff03c5ad49ba6ca548eeb51
Your Name <Ejhfast@gmail.com>
2011-10-28 17:59:52 -0700

    timed server

--- proofserver.hs
+++ proofserver.hs
@@ -8,6 +8,7 @@ import System.Timeout as S
 import ProofParse
 import ProofTypes
 import ProofSearch
+import System.IO.Unsafe
 
 main :: IO ()
 main = simpleHTTP nullConf $ app
@@ -30,22 +31,22 @@ process = do
     (Right r, Right a) ->
       let pres = parse proof "" p in
       case pres of
-        Right pstmts ->
-          let res = do_proof r a pstmts "" in
+        Right pstmts -> do
+          res <- do_proof r a pstmts ""
           ok $ toResponse res
         Left e -> ok $ toResponse (show e)
     _ -> ok $ toResponse "bad parse in assumptions/rulesets" --problem parsing
 
-do_proof :: [Ruleset String] -> [Expr String] -> [ProofStmt] -> String -> String
-do_proof _ _ [] msg = msg
-do_proof rs as (p:ps) msg =
-  let try_prove = checkproof 4 (stmt p) rs as in
+do_proof :: [Ruleset String] -> [Expr String] -> [ProofStmt] -> String -> ServerPartT IO String
+do_proof _ _ [] msg = do { return msg }
+do_proof rs as (p:ps) msg = do
+  try_prove <- checkproof 2 (stmt p) rs as
   case try_prove of
     Just x ->
       case verify_rules_assumptions x (r_deps p) (a_deps p) of
-        True -> do_proof rs as ps $ msg ++ "Proved "++(nm p)++"\n"
-        False -> msg ++ "Attempt to prove statement "++(nm p)++" with "++(show (r_deps p))++"... failed.\n"
-    _ -> msg ++ "Attempt to prove statement "++(nm p)++"... failed.\n"
+        True -> do_proof rs as ps (msg ++ "Proved "++(nm p)++"\n")
+        False -> return $ msg ++ "Attempt to prove statement "++(nm p)++" with "++(show (r_deps p))++"... failed.\n"
+    _ -> return $ msg ++ "Attempt to prove statement "++(nm p)++"... failed.\n"
 
 verify_rules_assumptions :: [Expr String] -> [String] -> [String] -> Bool
 verify_rules_assumptions exprs [] [] = True
@@ -60,16 +61,23 @@ match_d f (x:xs) m =
   let ms = filter (\f-> f /= "") $ sort m in
   if ms == rules then True else match_d f xs m
 
-f_search :: Int -> Stmt String -> [Expr String] -> [Ruleset String] -> [Expr String] -> Maybe [Expr String]
-f_search 0 _ _ _ stmts = Nothing
+f_search :: Int -> Stmt String -> [Expr String] -> [Ruleset String] -> [Expr String] -> IO (Maybe [Expr String])
+f_search 0 _ _ _ stmts = do {return $ Nothing }
 f_search depth start toprove rulesets stmts = 
   let update = stmts ++ apply_rulesets_stmts stmts rulesets in
   let res = [Expr "_" start (Just ((rule_deps x)++(rule_deps y)), Just (merge_deps (deps x) (deps y))) | (x,y) <- (contains toprove update)] in
   case res of 
-    (x:rst) -> Just res
+    (x:rst) -> do {return $ Just res}
     _ -> f_search (depth - 1) start toprove rulesets $ List.nub update
+    
+timed_search :: Int -> Stmt String -> [Expr String] -> [Ruleset String] -> [Expr String] -> (Maybe [Expr String])
+timed_search depth start toprove rulesets stmts = 
+  let search = unsafePerformIO $ S.timeout 100000 $ f_search depth start toprove rulesets stmts in
+  case search of
+    Just x -> x
+    Nothing -> Nothing
 
-checkproof :: Int -> Stmt String -> [Ruleset String] -> [Expr String] -> (Maybe [Expr String])
+checkproof :: Int -> Stmt String -> [Ruleset String] -> [Expr String] -> ServerPartT IO (Maybe [Expr String])
 checkproof depth stmt rulesets assumps =
   let equiv = backward_search 1 (Expr "_" stmt (Nothing,Nothing)) assumps rulesets in -- find things equivalent to the goal
-  f_search depth stmt equiv rulesets assumps
+  do { return $ timed_search depth stmt equiv rulesets assumps }
