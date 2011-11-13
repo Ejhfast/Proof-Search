ff35272adbd5cf733f8eefa3aba787a6e61b1e8c
Your Name <Ejhfast@gmail.com>
2011-11-13 09:00:42 -0800

    fix basic tests

--- ProofTest.hs
+++ ProofTest.hs
@@ -34,26 +34,24 @@ time_test to_prove rulesets stmts = do
     Nothing -> return "failed"
 
 test1 =
-  let free = make_ruleset "Free" ["#(A&B)"] "" in
-  let r1 = make_ruleset "Neg" ["~(~A)-->A",""] "" in
-  let r2 = make_ruleset "TP" ["A&(A=>B)~>B"] "" in
+  let r1 = make_ruleset "Neg" ["~(~A)-->A","(A,B)~>A&B"] "" in
+  let r2 = make_ruleset "TP" ["A,(A=>B)~>B"] "" in
   let s1 = make_expr "A1" "~(~Z)=>F" in
   let s2 = make_expr "A2" "F=>D" in
   let s3 = make_expr "A3" "Z" in
   let to_prove = make_stmt "Z&(Z=>F)" in
-  time_test to_prove [r1,r2,free] [s1,s2,s3]
+  time_test to_prove [r1,r2] [s1,s2,s3]
    
 test2 =
-  let r1 = make_ruleset "Free" ["#(A&B)"] ""  in
   let r2 = make_ruleset "DL" ["~(A&B)-->~A|~B","~(A|B)-->~A&~B"] ""  in
-  let r3 = make_ruleset "TP" ["A&(A=>B)~>B","(A=>B)&(B=>C)~>(A=>C)"] ""  in
+  let r3 = make_ruleset "TP" ["A,(A=>B)~>B","(A=>B),(B=>C)~>(A=>C)"] ""  in
   let s1 = make_expr "A1" "~(A&B)=>C" in
   let s2 = make_expr "A2" "C=>D" in
   let to_prove = make_stmt "~A|~B=>C" in
-  time_test to_prove [r1,r2,r3] [s1,s2]
+  time_test to_prove [r2,r3] [s1,s2]
   
 test3 = 
-  let r1 = make_ruleset "Free" ["#(A&B)"] ""  in
+  let r1 = make_ruleset "Free" ["(A,B)~>A&B"] ""  in
   let s1 = make_expr "A1" "A" in
   let s2 = make_expr "A2" "B" in
   let s3 = make_expr "A3" "C" in
@@ -63,30 +61,29 @@ test3 =
   time_test to_prove [r1] [s1,s2,s3,s4,s5]
   
 test4 =
-  let r1 = make_ruleset "Free" ["#(A&B)"] ""  in
   let r2 = make_ruleset "DL" ["~(A&B)-->~A|~B","~(A|B)-->~A&~B"] ""  in
   let s1 = make_expr "A1" "A&~(B|C)=>C" in
   let to_prove = make_stmt "A&(~B&~C)=>C" in
-  time_test to_prove [r1,r2] [s1]
+  time_test to_prove [r2] [s1]
   
 test5 =
-  let fr = make_ruleset "Free" ["#(A&B)","~(~A)-->A","(Q=>A&C)-->(Q=>A)","A&B-->B&A"] ""  in
-  let tp = make_ruleset "TP" ["A&(A=>B)-->B"] ""  in
+  let fr = make_ruleset "Free" ["~(~A)-->A","(Q=>A&C)-->(Q=>A)","A&B-->B&A"] ""  in
+  let tp = make_ruleset "TP" ["A,(A=>B)-->B"] ""  in
   let dl = make_ruleset "DL" ["~(A&B)-->~A|~B","~(A|B)-->~A&~B"] "" in
   let s1 = make_expr "A1" "~K=>~(Q|M)" in
   let to_prove = make_stmt "~K=>~Q" in
   time_test to_prove [dl,fr,tp] [s1]
   
 test6 =
-  let r1 = make_ruleset "Free" ["~(~A)-->A","A-->~(~A)","A,B-->B,A","A,B-->A&B"] ""  in
+  let r1 = make_ruleset "Free" ["~(~A)-->A","A-->~(~A)"] ""  in
   let r2 = make_ruleset "DL" ["~(A&B)-->~A|~B","~(A|B)-->~A&~B"] ""  in
   let s1 = make_expr "A1" "~(A&(B|C))=>C" in
   let to_prove = make_stmt "~A|(~B&~C)=>C" in
   time_test to_prove [r1,r2] [s1]
 
 test7 =
-  let r1 = make_ruleset "Free" ["#(A&B)","#(A&B&C)","A&B-->B&A"] ""  in
-  let r2 = make_ruleset "TP" ["A&(A=>B)-->B"] ""  in
+  let r1 = make_ruleset "Free" ["A,B-->B&A"] ""  in
+  let r2 = make_ruleset "TP" ["A,(A=>B)-->B"] ""  in
   let s1 = make_expr "A1" "A=>B" in
   let s2 = make_expr "A2" "B=>C" in
   let s3 = make_expr "A3" "C=>D" in
@@ -150,9 +147,6 @@ test16 =
   let s2 = make_expr "A2" "(D&F&G)=>~B" in
   let to_prove = make_stmt "~A=>~B" in
   time_test to_prove [r1] [s1,s2] 
-  --let ss = 
-  --apply_rulesets_stmts [s1,s2] [r1] --in
-  --apply_rulesets_stmts ss [r1]
   
 -- bad tests, mostly testing "general rewrites vs. equality"
 
