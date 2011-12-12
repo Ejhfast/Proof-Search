8edecb8e89c4fd088a79b85129c40d7fc028309d
Your Name <Ejhfast@gmail.com>
2011-12-12 13:35:52 -0800

    parse proof objects

--- NewParse.hs
+++ NewParse.hs
@@ -2,78 +2,221 @@ module ProofParse where
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import ProofTypes
-import Data.String.Utils
+import Data.String.Utils as Data
 import Control.Monad
 import List
 
+-- Define Proof Data Types
+
+data ProofLine = ProofLine {proof_name :: String, statement :: Stmt String, from_rules :: [String], with_assumps :: [String]}
+  deriving (Eq,Show)
+
+-- Helpers
+
+remove_ws :: String -> String
+remove_ws str =
+  let sp_str = zip (split "\"" str) [0..] in
+  Data.join "\"" [if (even i) then filter (\s -> s /= ' ') x else x | (x,i) <- sp_str]
+
+-- Type helpers
+
+-- Turn argument list into binary tree
+argument_tree :: [Stmt String] -> Stmt String
+argument_tree (x:xs) =
+  case xs of
+    [] -> Op "ARGS" x (Var "NOP")
+    _ -> Op "ARGS" x (argument_tree xs)
+
+func_tree :: String -> Stmt String -> Stmt String -> Stmt String    
+func_tree op a b = Op op a b
+
+unary_tree :: String -> Stmt String -> Stmt String
+unary_tree op a = Op op a (Var "NOP")
+
+make_rule :: Stmt String -> Rule String
+make_rule stmt =
+  case stmt of
+    (Op "eq_rewrite" a b) -> Rule a b Equality
+    (Op "rewrite" a b) -> Rule a b Strict
+    _ -> Rule (Var "NOP") (Var "NOP") Strict -- fail...
+    
+do_parse_rule :: String -> Rule String
+do_parse_rule str =
+  let stmt = parse (run_parse "free") "" $ remove_ws str in
+  case stmt of
+    Right a -> make_rule a
+    _ -> Rule (Free "A") (Free "A") Strict -- fail...
+    
+
 -- Tex infastructure
 
-parse_tex_command :: String -> Int -> Parser String -> Parser String
+parse_tex_command :: String -> Int -> Parser (Stmt String) -> Parser (Stmt String)
 parse_tex_command command args parse_rest = do
-	c <- string command
+	c <- string $ "\\" ++ command
 	m <- modifiers parse_rest
 	get_args <- count args $ arg_parse parse_rest
-	return c
+	return $ Op command (argument_tree get_args) m
 
-arg_parse :: Parser String -> Parser String
+arg_parse :: Parser (Stmt String) -> Parser (Stmt String)
 arg_parse parse_rest = 
   do { char '{'; x <- parse_rest; char '}'; return x}
 
-underscore :: Parser String -> Parser String
+underscore :: Parser (Stmt String) -> Parser (Stmt String)
 underscore parse_rest =
   do { string "_{"; x <- parse_rest; char '}'; return x}
 
-carrot :: Parser String -> Parser String
+carrot :: Parser (Stmt String) -> Parser (Stmt String)
 carrot parse_rest =
   do { string "^{"; x <- parse_rest; char '}'; return x}
   
-modifiers :: Parser String -> Parser String
+modifiers :: Parser (Stmt String) -> Parser (Stmt String)
 modifiers parse_rest = do
-  optional $ underscore parse_rest
-  optional $ carrot parse_rest
-  return "ok"
+  u <- optionMaybe $ underscore parse_rest
+  c <- optionMaybe $ carrot parse_rest
+  case (u,c) of
+    (Just uv, Just cv) -> return $ Op "Meta" uv cv
+    (Just uv, Nothing) -> return $ Op "Meta" uv (Var "NOP")
+    (Nothing, Just cv) -> return $ Op "Meta" (Var "NOP") cv
+    _ -> return $ Op "Meta" (Var "NOP") (Var "NOP")
 
-fake_parse :: Parser String
-fake_parse = string "0"
+fake_parse :: Parser (Stmt String)
+fake_parse = do { x <- string "0"; return (Var x) }
 
-tryall :: [Parser String] -> Parser String
+tryall :: [Parser (Stmt String)] -> Parser (Stmt String)
 tryall ps = foldr (\x -> (<|> (try x))) mzero ps
 
-recurse :: Parser String
-recurse = try fake_parse <|> all_funcs
+-- Main calls here to parse out baby latex expressions
 
-all_funcs :: Parser String
-all_funcs = tryall [x recurse | x <- [go,lala,eq_rw,rw]]
+recurse :: String -> Parser (Stmt String)
+recurse kind = try (expr (all_funcs kind) kind) <|> all_funcs kind
 
--- Basic Expressions
--- \eq_rewrite{A+B}{B+A}
--- \rewrite{A+B}{B+A}
+all_funcs :: String -> Parser (Stmt String)
+all_funcs kind = tryall [x $ recurse kind | x <- [go,lala,eq_rw,rw]]
 
--- expr = buildExpressionParser table primative <?> "expression"
--- primative = many1 digit
--- 
--- table = [
---     [prefix "~" neg]
---   , [op "." (sop_gen ".") AssocRight]
---   , [op "&" (sop_gen "&") AssocLeft, op "|" (sop_gen "|") AssocLeft, op "," (sop_gen ",") AssocLeft]
---   , [op "*" (sop_gen "*") AssocLeft, op "/" (sop_gen "/") AssocLeft]
---   , [op "+" (sop_gen "+") AssocLeft, op "-" (sop_gen "-") AssocLeft]
---   , op "=" (sop_gen "=") AssocLeft]
---   where
---     op s f assoc = Infix (do { string s; return f }) assoc
---     prefix name fun = Prefix (do{ string name; return fun })
+-- Parse out rulesets
+
+parse_rule :: Parser (Rule String)
+parse_rule = do
+  x <- recurse "free"; -- A rule is just a certain type of statement in the language
+  return $ make_rule x
+
+parse_ruleset :: Parser (Ruleset String)
+parse_ruleset = do
+  name <- get_symbol;
+  string ":{\"";
+  descrip <- get_description;
+  string "\";";
+  rules <- sepBy parse_rule (char ';');
+  string "}";
+  return $ Ruleset name rules descrip
+  
+parse_rulesets :: Parser [Ruleset String]
+parse_rulesets = many1 parse_ruleset
+  
+parse_assumption :: Parser (Expr String)
+parse_assumption = do
+  name <- get_symbol;
+  char ':';
+  expr <- recurse "ground";
+  char ';'
+  return $ Expr name expr (Nothing,Nothing)
+
+parse_assumptions :: Parser [Expr String]
+parse_assumptions = many1 parse_assumption
+
+parse_proofline :: Parser ProofLine
+parse_proofline = do
+  name <- get_symbol;
+  char ':';
+  conclusion <- recurse "ground";
+  char ';';
+  rules <- optionMaybe $ with_semi stringlist;
+  assumptions <- optionMaybe $ with_semi stringlist;
+  case (rules, assumptions) of
+    (Just r, Just a) -> return $ ProofLine name conclusion r a
+    (Just r, Nothing) -> return $ ProofLine name conclusion r []
+    (Nothing, Just a) -> return $ ProofLine name conclusion [] a
+    _ -> return $ ProofLine name conclusion [] []
+    
+parse_proof :: Parser [ProofLine]
+parse_proof = many1 parse_proofline
+  
+
+-- Primative Expressions
+
+with_semi p = do
+  x <- p;
+  char ';'
+  return x;
+
+stringlist :: Parser [String]
+stringlist = do
+  char '[';
+  items <- sepBy get_symbol (char ',');
+  char ']';
+  return items
+
+get_description :: Parser String
+get_description = do
+  ws <- sepBy get_symbol (string " ");
+  return $ foldr (\x y -> x ++ " " ++ y) "" ws
+
+get_symbol :: Parser String
+get_symbol = many1 letter
+
+declared_constant :: Parser (Stmt String)
+declared_constant = do {char '$'; x <- get_symbol; return (Var x)} <?> "constant"
+
+number :: Parser (Stmt String)
+number = do {x <- many1 digit; return (Var x)}
+
+symbol :: String -> Parser (Stmt String)
+symbol kind = do
+  x <- get_symbol
+  m <- modifiers $ recurse kind
+  case kind of
+    "free" -> return $ Op "Symbol" (Free x) m
+    "ground" -> return $ Op "Symbol" (Var x) m
+
+factor :: Parser (Stmt String) -> String -> Parser (Stmt String)
+factor tex_parse kind = do {char '('; x <- expr tex_parse kind; char ')'; return x }
+  <|> declared_constant
+  <|> number
+  <|> symbol kind
+  
+expr tex_parse kind = buildExpressionParser table (new_p kind) <?> "expression"
+  where new_p kind = tex_parse <|> (factor tex_parse kind)
+
+table = [
+    [prefix "~" (unary_tree "~")]
+  , [op "." (func_tree ".") AssocRight]
+  , [op "&" (func_tree "&") AssocLeft, op "|" (func_tree "|") AssocLeft, op "," (func_tree ",") AssocLeft]
+  , [op "*" (func_tree "*") AssocLeft, op "/" (func_tree "/") AssocLeft]
+  , [op "+" (func_tree "+") AssocLeft, op "-" (func_tree "-") AssocLeft]
+  , [op "=" (func_tree "=") AssocLeft]]
+  where
+    op s f assoc = Infix (do { string s; return f }) assoc
+    prefix name fun = Prefix (do{ string name; return fun })
 
 -- Test
 
-lala = parse_tex_command "\\lala" 3
-go = parse_tex_command "\\go" 2
-eq_rw = parse_tex_command "\\eq_rewrite" 2
-rw = parse_tex_command "\\rewrite" 2
+lala = parse_tex_command "lala" 3
+go = parse_tex_command "go" 2
+eq_rw = parse_tex_command "eq_rewrite" 2
+rw = parse_tex_command "rewrite" 2
 
-
-run_parse = do
-	x <- all_funcs;
+run_parse kind = do
+	x <- recurse kind;
 	eof;
 	return x
+
+ex_rule = "\\rewrite{\\go{1}{1}}{\\go{0}{0}+\\go{A_{1}^{2}}{0}}"
+ex_ruleset = "myfule:{\"ok go\";"++ex_rule++";"++ex_rule++";"++ex_rule++"}"
+test_parse = parse (run_parse "ground") "" ex_rule
+test_ruleset = parse parse_ruleset "" $ ex_ruleset
+test_rulesets = parse parse_rulesets "" $ ex_ruleset++ex_ruleset++ex_ruleset
+test_rule = case test_parse of 
+  Right x -> Just (make_rule x)
+  _ -> Nothing
 
   
---
+++ NewServer.hs
@@ -0,0 +1,125 @@
+module Main where
+import Control.Monad (msum)
+import Happstack.Server
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import List
+import System.Timeout as S
+import NewParse
+import ProofTypes as PT
+import ProofSearch
+import Text.JSON
+import System.IO.Unsafe
+
+main :: IO ()
+main = simpleHTTP nullConf $ app
+
+app :: ServerPart Response
+app = do 
+  decodeBody (defaultBodyPolicy "/tmp/" 0 10000 10000) 
+  msum [   dir "check_proof" $ check_proof,
+           dir "check_assignment" $ check_assign ]
+           
+check_proof :: ServerPart Response
+check_proof = do 
+  methodM POST
+  rs <- look "rulesets"
+  as <- look "assumptions"
+  reqs <- look "reqs"
+  p <- look "proof"
+  goal <- look "goal"
+  let rs_parsed = parse rulesets "" $ remove_ws rs
+  let as_parsed = parse assumptions "" $ remove_ws as
+  let g_parsed = parse (expr "stmt") "" $ remove_ws goal
+  case (rs_parsed, as_parsed, g_parsed) of 
+    (Right r, Right a, Right g) ->
+      let pres = parse proof "" $ remove_ws p in
+      case pres of
+        Right pstmts -> do
+          res <- do_proof r a pstmts g []
+          ok $ toResponse res
+        Left e -> ok $ toResponse (show e)
+    _ -> ok $ toResponse "problem parsing"
+    
+check_assign :: ServerPart Response
+check_assign = do
+  methodM POST
+  rs <- look "rulesets"
+  as <- look "assumptions"
+  g <- look "goal"
+  let rs_parsed = parse rulesets "" $ remove_ws rs
+  let as_parsed = parse assumptions "" $ remove_ws as
+  let g_parsed = parse (expr "stmt") "" $ remove_ws g
+  case (rs_parsed, as_parsed, g_parsed) of 
+    (Right r, Right a, Right go) ->
+      ok $ toResponse $ foldr (++) [] [pretty_ruleset x | x <- r]
+    (p1,p2,p3) -> ok $ toResponse $ "Fail: bad parse in assumptions/rulesets\n"++(show p1)++"\n"++(show p2)++"\n"++(show p3)
+
+proved :: [(String,(JSObject String))] -> ProofStmt -> [String] -> [String] -> [(String,(JSObject String))]
+proved msg pstmt rules assums = ((nm pstmt),attrs):msg
+  where attrs = toJSObject [("status","proved"),("rules",(show rules)),("assumptions",(show assums))]
+  --msg++"Proved "++(nm pstmt)++" with "++(show rules)++" and assumptions "++(show assums)++".\n"
+failed :: [(String,(JSObject String))] -> ProofStmt -> [Expr String] -> [(String,(JSObject String))]
+failed msg pstmt [] = ((nm pstmt),attrs):msg
+  where attrs = toJSObject [("status","failed")]
+failed msg pstmt (h:hs) = ((nm pstmt),attrs):msg
+  where attrs = toJSObject [("status","failed"),("hint_rules",(show $ rule_deps h)),("hint_assumps",(show $ deps h))]
+  
+  --msg++"Failed to prove "++(nm pstmt)++" with "++(show $ r_deps pstmt)++" and assumptions "++(show $ a_deps pstmt)++".\n"
+  
+pretty_ruleset :: Ruleset String -> String
+pretty_ruleset ruleset =
+  (show $ name ruleset)++": "++(show $ descrip ruleset)++"\n" 
+
+do_proof :: [Ruleset String] -> [Expr String] -> [ProofStmt] -> Stmt String -> [(String,(JSObject String))] -> ServerPartT IO String
+do_proof _ _ [] _ msg = do { return $ encode $ toJSObject msg }
+do_proof rs as (p:ps) goal msg = do
+  let use_rules = if (r_deps p) == [] then rs else filter (\r -> ((name r) `elem` (r_deps p)) || (name r) == "Free" ) rs
+  let use_assumps = if (a_deps p) == [] then as else filter (\a -> (_id a) `elem` (a_deps p)) as
+  try_prove <- checkproof 3 (stmt p) use_rules use_assumps
+  case try_prove of
+    Just (x:xs) ->
+      case verify_rules_assumptions (x:xs) (r_deps p) (a_deps p) of
+        True -> let newassum = Expr (nm p) (stmt p) (Nothing,Nothing) in
+          if (PT.body x) == goal then return $ encode $ toJSObject $ (proved msg p (rule_deps x) (deps x))
+            else do_proof rs (newassum:as) ps goal (proved msg p (rule_deps x) (deps x))
+        False -> return $ encode $ toJSObject $ failed msg p []
+    _ -> do
+      try_prove <- checkproof 5 (stmt p) rs as
+      case try_prove of
+        Just x -> return $ encode $ toJSObject $ failed msg p x
+        otherwise -> return $ encode $ toJSObject $ failed msg p []
+
+verify_rules_assumptions :: [Expr String] -> [String] -> [String] -> Bool
+verify_rules_assumptions exprs [] [] = True
+verify_rules_assumptions exprs r_d [] = match_d rule_deps exprs r_d
+verify_rules_assumptions exprs [] a_d = match_d deps exprs a_d
+verify_rules_assumptions exprs r_d a_d = (match_d deps exprs a_d) && (match_d rule_deps exprs r_d)
+
+match_d :: (Expr String -> [String]) -> [Expr String] -> [String] -> Bool
+match_d f [] m = False
+match_d f (x:xs) m = 
+  let rules = sort $ filter (\x -> x /= "Free" && x /= "_") (f x) in
+  let ms = filter (\f-> f /= "" && f /= "Free") $ sort m in
+  if ms == rules then True else match_d f xs m
+
+f_search :: Int -> Stmt String -> [Expr String] -> [Ruleset String] -> [Expr String] -> IO (Maybe [Expr String])
+f_search 0 _ _ _ stmts = do {return $ Nothing }
+f_search depth start toprove rulesets stmts = 
+  let update = stmts ++ apply_rulesets_stmts stmts rulesets in
+  let res = [Expr "_" start (Just ((rule_deps x)++(rule_deps y)), Just (merge_deps (deps x) (deps y))) | (x,y) <- (contains toprove update)] in
+  case res of 
+    (x:rst) -> do {return $ Just res}
+    _ -> f_search (depth - 1) start toprove rulesets update
+    
+timed_search :: Int -> Stmt String -> [Expr String] -> [Ruleset String] -> [Expr String] -> (Maybe [Expr String])
+timed_search depth start toprove rulesets stmts = 
+  let search = unsafePerformIO $ S.timeout 1000000 $ f_search depth start toprove rulesets stmts in
+  case search of
+    Just x -> x
+    Nothing -> Nothing
+
+checkproof :: Int -> Stmt String -> [Ruleset String] -> [Expr String] -> ServerPartT IO (Maybe [Expr String])
+checkproof depth stmt rulesets assumps =
+  let equiv = backward_search 1 (Expr "_" stmt (Nothing,Nothing)) assumps rulesets in -- find things equivalent to the goal
+  do { return $ timed_search depth stmt equiv rulesets assumps }
