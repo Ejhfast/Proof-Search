c830988999be0dda801dac0d026d7edb2aa2280c
James Michael DuPont <jamesmikedupont@gmail.com>
2013-11-30 08:48:39 -0600

    more tests

--- testFrame2.hs
+++ testFrame2.hs
@@ -7,6 +7,137 @@ import ProofTypes
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Error
 import Text.ParserCombinators.Parsec.Expr
+import ProofTypes as PT
+
+-------------------------------------------
+maybeParse :: Parser [a]
+               -> String
+               -> Maybe [a]
+maybeParse parser str =
+  if str == "" then Just [] else
+    let tryit = parse parser "" str in
+    case tryit of
+      (Right res) -> Just res
+      _ -> Nothing
+
+-- here we define a set of symbols 
+texSymbols = [
+  ( "go", 2 ),
+  ( "rewrite", 2 ) ,
+  ("star" , 1) ,
+  ("e" , 0)
+  ]
+
+
+-----------------------------------------
+-- 1 rulesets <- look "rulesets"
+-- 2 frees <- look "frees"
+-- 3 assumptions <- look "assumptions"
+-- 4 conclusion <- look "conclusion"
+-- 5 syntax <- look "syntax" ( an array of tokens)
+-------------------------------------------
+iter :: Int -> [Ruleset String] ->
+        [Ruleset String] ->
+        [Expr String] ->
+        [Expr String] -> String
+iter 0 _ _ _ _ = "Failed to prove."
+iter depth rsets fsets assumps conc =
+  let back = (++) conc $
+             backApplyRulesetsStmts
+             conc fsets in -- Look backward once with frees
+  let expand_back = (++)
+                    back $
+                    backApplyRulesetsStmts
+                    back rsets in -- Generate one layer back from conclusion
+  let fwrd = (++)
+             assumps $
+             applyRulesetsStmts assumps fsets in -- Look forward with frees
+  let expand_fwrd = (++)
+                    fwrd $ applyRulesetsStmts fwrd rsets
+  in -- Generate one layer forward from assumptions
+   let matches = [(x , y) |
+                  x <- expand_fwrd,
+                  y <- expand_back,
+                  PT.body x == PT.body y] in
+   case matches of
+     (x : rst) ->  "Proved"
+     _ -> iter (depth - 1) rsets fsets fwrd back
+
+-------------------------------------------
+doProof :: String ->
+           String ->
+           String ->
+           String ->
+           String ->
+           String
+doProof     rulesets frees   assumptions  conclusion  syntax = do
+  let new_parse_funcs =
+        if syntax == ""
+        then []
+        else read syntax :: [(String, Int)]
+  let try_rulesets =
+        maybeParse (parseRulesets new_parse_funcs)
+        $ removeWs rulesets
+  case try_rulesets of
+    Just r ->
+      let try_frees = maybeParse
+                      (parseRulesets new_parse_funcs) $
+                      removeWs frees in
+      case try_frees of
+        Just f ->
+          let try_assumps =
+                maybeParse
+                (parseAssumptions new_parse_funcs) $
+                removeWs assumptions in
+          case try_assumps of
+            Just a ->
+              let try_conc =
+                    maybeParse (parseConclusion
+                                new_parse_funcs)
+                    $ removeWs conclusion in
+              case try_conc of
+                Just c -> do
+                  foo <- iter 2 r f a c
+                  return foo
+                _ -> "Failure: Error parsing conclusion"
+            _ -> "Failure: Error parsing assumptions"
+        _ -> "Failure: Error parsing free rulesets"
+    _ -> "Failure: Error parsing required rulesets"
+
+-------------------------------------------
+runAssign :: String ->
+             String ->
+             String ->
+             String ->
+             -- return
+             String 
+runAssign  rulesets
+           assumptions
+           goal
+           syntax
+  = do
+  let new_parse_funcs =
+        if syntax == ""
+        then []
+        else read syntax :: [(String, Int)]
+  let rs_parsed = parse (parseRulesets new_parse_funcs)
+                  "" $ removeWs rulesets
+  let as_parsed = parse (parseAssumptions new_parse_funcs)
+                  "" $ removeWs assumptions
+  let g_parsed = parse (recurse "ground" new_parse_funcs)
+                 "" $ removeWs goal
+  case (rs_parsed, as_parsed, g_parsed) of
+    (Right r, Right a, Right go) ->
+      "Success: Parsed assignment."
+    (p1,
+     p2,
+     p3) ->
+      "Failure: bad parse in assumptions/rulesets." ++
+      show p1
+      ++ show p2 ++
+      show p3
+      
+-------------------------------------------
 
 testRuleset = parse (parseRuleset [
                         ( "go", 2 ),
@@ -50,12 +181,17 @@ fakeParse = do {
   return (Var x)
   }
 
-  -- allFuncs
+
+
   -- argParse
   -- carrot
   -- constraint
   -- constraints
-  -- createCommands
+            
+  -- parseTexCommand, called by createCommands
+  -- createCommands called by allFuncs
+  -- allFuncs called by recurse
+            
   -- factor
   -- modifiers
   -- number
@@ -64,8 +200,8 @@ fakeParse = do {
   -- parseConclusion
   -- parseRule
   -- parseRuleset
-  -- parseTexCommand
-  -- recurse
+
+  -- recurse called by parseAssumption, and parseRule, symbol
   -- symbol
   -- tryall
   -- underscore
@@ -96,7 +232,7 @@ commutativity_x = Right (
      kind = Strict,
      cnst = []})
 
-unfold        = parse (     parseRule []     ) "" "star{A} := (A. star{A}) + e "  
+unfold        = parse (     parseRule texSymbols     ) "" "\\star{A} := (A. \\star{A}) + e "  
 right_identity= parse (     parseRule []     ) "" "A.e := A"  
 left_identity = parse (     parseRule []     ) "" "e.A := A"  
 absorption    = parse (     parseRule []     ) "" "A + e := e"  
@@ -110,10 +246,15 @@ selectb       = parse (     parseRule []     ) ""  "A + B := A"
 -- Right (Rule {condition = Var "NOP", conclusion = Var "NOP", kind = Strict, cnst = []})
 
 
-tex_star = '\\star'
+
+--parse_tex_star  = parse ( parseTexCommand texSymbols) ""  "\\star" 2 
+example_ruleset = parse (parseRulesets texSymbols)
+                 "" "Test{A=B;C=D;}"
+                  
+
 
 main2 = do
-  putStrLn ("this is a test: "  );
+  putStrLn ("parseAssumptions: "  );
   putStrLn (show (  parse (     parseAssumptions []     ) "" "a:1;"  ));
 
   putStrLn ("commutativity: "  );
@@ -135,6 +276,12 @@ main2 = do
   putStrLn ("selectb: "  );
   putStrLn (show ( selectb      ));
   
+
+--  putStrLn ("parse tex star: "  );
+--  putStrLn (show (   parse_tex_star      ));
+
+  putStrLn ("example ruleset: "  );
+  putStrLn(show(example_ruleset));
 
   
 --    putStrLn (show (testParseRuleset));
